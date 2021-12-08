48c1c5cce1826eb6138f8b5f01f63bcf6568a3ab
Bahamamama3 <kuzebaevk@gmail.com>
2021-12-08 19:52:37 +0600

    add placeholder for TextView

--- Todoey/Controllers/TextViewController.swift
+++ Todoey/Controllers/TextViewController.swift
@@ -11,16 +11,30 @@ class TextViewController: UIViewController {
     var selectedItem: Item!
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
-    @IBOutlet private var textView: UITextView!
+    @IBOutlet private var textView: UITextView! 
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        textView.text = selectedItem.desc
+
+        if let desc = selectedItem.desc, !desc.isEmpty {
+            textView.text = desc
+        } else {
+            textView.text = "Добавьте описание"
+            textView.textColor = UIColor.lightGray
+        }
     }
 }
 
 extension TextViewController: UITextViewDelegate {
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+
+        if textView.textColor == UIColor.lightGray {
+            textView.text = nil
+            textView.textColor = UIColor.black
+        }
+    }
+    
     func textViewDidEndEditing(_ textView: UITextView) {
         
         selectedItem.desc = textView.text
